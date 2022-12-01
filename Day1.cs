7d476553cd0ecd2618080c9a8f5e8256aa6faf26
Lucas <29230467+Somfic@users.noreply.github.com>
2022-12-01 20:07:42 +0100

    Solved day 1

---
+++ Day1.cs
@@ -0,0 +1,20 @@
+﻿class Day1 {
+	public static void Solve() {
+		var lines = File.ReadAllLines(Path.Join("inputs", "day1"));
+		var elves = new List<int>() { 0 };
+
+		foreach (var line in lines)
+		{
+			if (string.IsNullOrWhiteSpace(line))
+				elves.Add(0);
+			else
+				elves[elves.Count - 1] += int.Parse(line);
+		}
+
+		var topElf = elves.Max();
+		var topThreeElves = elves.OrderByDescending(e => e).Take(3).Sum();
+
+		Console.WriteLine($"Top Elf: {topElf}");
+		Console.WriteLine($"Top Three Elves: {topThreeElves}");
+	}
+}
