e6cfd804bbd3b2769416455aaa2bb3a8535ebfc6
Lucas <29230467+Somfic@users.noreply.github.com>
2022-12-05 00:56:22 +0100

    Prettier result output

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-Day2.SolvePartOne();
-Day2.SolvePartTwo();
+using advent_of_code;
+
+var days = new Day[] { new Day1(), new Day2() };
+
+for (var index = 0; index < days.Length; index++)
+{
+    var day = days[index];
+    var (partOne, partTwo) = day.Solve();
+    
+    Console.WriteLine($"{index + 1}.1: {partOne}");
+    Console.WriteLine($"  2: {partTwo}");
+    Console.WriteLine();
+}
